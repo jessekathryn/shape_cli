41b9c803e81b4bc36ef75bfecccc49790915d165
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 19:06:22 +0000

    display_articles - CLI

--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -14,5 +14,23 @@ class ShapeCli::CLI
    |________________________________________________________________________|" 
     
   end
+  
+  def display_articles
+    
+    puts 
+    "
+                                   SHAPE CLI 
+    ________________________________________________________________________
+   |                                                                        |
+   |    Enter a number from the list of latest popular topics in Lifestyle  |
+   |    below to view more details                                          |
+   |                                                                        |
+   |    |1|   5 Ways to Change Your Life -- For Good                        |
+   |    |2|   What Really Helps Get Rid of Cellulite?                       |  
+   |    |3|   10 Simple Rules for a Healthy Life                            |  
+   |    |4|   3 Cool Winter Hairstyles                                      |
+   |    |5|   7 Mind Tricks for Self-Motivation                             |               
+   |________________________________________________________________________|"
+  end
 end
   
