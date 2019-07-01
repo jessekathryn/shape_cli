ef289c46918fca940f7ba084a2e135e6ce80a503
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 20:29:55 +0000

    exit method created

--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -2,51 +2,59 @@ class ShapeCli::CLI
   
   def call
     puts "
-                                  SHAPE CLI 
-    ________________________________________________________________________
-   |                                                                        |
-   | Shape CLI is a quick source for the latest popular topics in Lifestyle.|
-   |                                                                        |
-   | Want tips and info on beauty, fashion, travel, health, sex, love and   |
-   | everything else you need to live a fuller and happier life?            |
-   |                                                                        |
-   |________________________________________________________________________|" 
-   menu
+                                SHAPE CLI 
+ ________________________________________________________________________
+|                                                                        |
+| Shape CLI is a quick source for the latest popular topics in Lifestyle.|
+|                                                                        |
+| Want tips and info on beauty, fashion, travel, health, sex, love and   |
+| everything else you need to live a fuller and happier life?            |
+|                                                                        |
+|________________________________________________________________________|" 
+    menu
+    exit
   end
   
   def display_articles
     puts "
-                                   SHAPE CLI 
-    ________________________________________________________________________
-   |                                                                        |
-   |    Enter a number from the list of latest popular topics in Lifestyle  |
-   |    below to view more details                                          |
-   |                                                                        |
-   |    |1|   5 Ways to Change Your Life -- For Good                        |
-   |    |2|   What Really Helps Get Rid of Cellulite?                       |  
-   |    |3|   10 Simple Rules for a Healthy Life                            |  
-   |    |4|   3 Cool Winter Hairstyles                                      |
-   |    |5|   7 Mind Tricks for Self-Motivation                             |               
-   |________________________________________________________________________|"
+                              SHAPE CLI 
+ ________________________________________________________________________ 
+|                                                                        |
+|    Enter a number from the list of latest popular topics in Lifestyle  |
+|    below to view more details                                          |
+|                                                                        |
+|    |1|   5 Ways to Change Your Life -- For Good                        |
+|    |2|   What Really Helps Get Rid of Cellulite?                       |  
+|    |3|   10 Simple Rules for a Healthy Life                            |   
+|    |4|   3 Cool Winter Hairstyles                                      |
+|    |5|   7 Mind Tricks for Self-Motivation                             |
+|________________________________________________________________________|"
   end
   
   def menu 
     puts "
-                                  SHAPE CLI 
-    _________________________________________________________________________
-   |                                                                         |
-   |     Enter Y/N  :                                                        |
-   |_________________________________________________________________________|"
-    input = gets.strip
-    case input
-    when "Y" 
-      puts display_articles
-    when "N"
-      puts "
-    _________________________________________________________________________
-   |                                                                         |
-   |  You will now exit! Thank you for trying Shape CLI! Have a nice day.    |
-   |_________________________________________________________________________|"
+                                SHAPE CLI 
+ _________________________________________________________________________
+|                                                                         |
+|     Enter Y/N  :                                                        |
+|_________________________________________________________________________|"
+   input = nil
+    while input != "exit" && input != "N"
+      input = gets.strip
+      case input
+      when "Y" 
+        puts display_articles
+      when "N" 
+        puts exit
+      end
     end
   end
+  
+  def exit
+   puts "
+ _________________________________________________________________________
+|                                                                         |
+|  You will now exit! Thank you for trying Shape CLI! Have a nice day~    |
+|_________________________________________________________________________|"
+  end 
 end
