96d49bef2e2cb169fc59aa97d18cb3834dbbd428
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 21:13:19 +0000

    article class method

---
+++ lib/shape_cli/article.rb
@@ -0,0 +1,18 @@
+class ShapeCli::Article
+  
+  def self.all 
+    puts "
+                              SHAPE CLI 
+ ________________________________________________________________________ 
+|                                                                        |
+|    Enter a number from the list of latest popular topics in Lifestyle  |
+|    below to view more details                                          |
+|                                                                        |
+|    |1|   5 Ways to Change Your Life -- For Good                        |
+|    |2|   What Really Helps Get Rid of Cellulite?                       |  
+|    |3|   10 Simple Rules for a Healthy Life                            |   
+|    |4|   3 Cool Winter Hairstyles                                      |
+|    |5|   7 Mind Tricks for Self-Motivation                             |
+|________________________________________________________________________|"
+  end
+end
--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -16,20 +16,7 @@ class ShapeCli::CLI
   end
   
   def display_articles
-    puts "
-                              SHAPE CLI 
- ________________________________________________________________________ 
-|                                                                        |
-|    Enter a number from the list of latest popular topics in Lifestyle  |
-|    below to view more details                                          |
-|                                                                        |
-|    |1|   5 Ways to Change Your Life -- For Good                        |
-|    |2|   What Really Helps Get Rid of Cellulite?                       |  
-|    |3|   10 Simple Rules for a Healthy Life                            |   
-|    |4|   3 Cool Winter Hairstyles                                      |
-|    |5|   7 Mind Tricks for Self-Motivation                             |
-|________________________________________________________________________|"
-    @articles = ShapeCli::Articles.all
+    @article = ShapeCli::Article.all
   end
   
   def menu 
