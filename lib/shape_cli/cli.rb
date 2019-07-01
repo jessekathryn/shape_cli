2a3749897b9cfdeec45960fc87eb5baf31ac30d2
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 02:15:42 +0000

    added method call and created new rb file cli

---
+++ lib/shape_cli/cli.rb
@@ -0,0 +1,12 @@
+#CLI Controller
+class ShapeCli::CLI 
+  
+  def call
+    puts "SHAPE CLI Shape CLI is a quick source for the latest popular topics in Lifestyle!    
+    
+    Want tips and info on beauty, fashion, travel, health, sex, love and everything else you need to live a fuller and happier life?
+
+    Yes or  No " 
+  end
+end
+  
