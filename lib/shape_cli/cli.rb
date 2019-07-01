ec97d5b3a81cc950625967893e9e67e27d92794a
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 03:46:57 +0000

    completed set-up for req. in bin/lib/shape_cli.rb and call method was successfully returned

--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -1,11 +1,7 @@
 class ShapeCli::CLI 
   
   def call
-    puts "SHAPE CLI Shape CLI is a quick source for the latest popular topics in Lifestyle!    
-    
-    Want tips and info on beauty, fashion, travel, health, sex, love and everything else you need to live a fuller and happier life?
-
-    Yes or  No " 
+    puts "SHAPE CLI Shape CLI is a quick source for the latest popular topics in Lifestyle!"
   end
 end
   
