b35b2b9b6e10cc8c4a255f99446ca126f1774b08
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 01:00:35 +0000

    reset project- ready to go

--- lib/shape_cli.rb
+++ lib/shape_cli.rb
@@ -1,16 +1,5 @@
 require "shape_cli/version"
 
-module ShapeCli #zerp following the set-up for article on how to build a gem https://robdodson.me/how-to-write-a-command-line-ruby-gem/
-  class ShapeConsole #chatter class name found on https://robdodson.me/how-to-write-a-command-line-ruby-gem/
+module ShapeCli 
   
-def say_hi
-  puts "SHAPE CLI 
-  
-  Shape CLI is a quick source for the latest popular topics in Lifestyle!    
-
-  Want tips and info on beauty, fashion, travel, health, sex, love and everything else you need to live a fuller and happier life?
-
-  Yes or  No"
-  end
- end
 end
