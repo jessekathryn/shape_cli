5f396a7d378d1a91a86115904c3196b6ccda4f9e
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-01 04:05:27 +0000

    edited call message

--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -4,13 +4,14 @@ class ShapeCli::CLI
     puts "
                                   SHAPE CLI 
     ________________________________________________________________________
-    Shape CLI - the quick source for the latest popular topics in Lifestyle!
-    
-    Want tips and info on beauty, fashion, travel, health, sex, love and 
-    everything else you need to live a fuller and happier life?
-
-    Enter Y/N 
-    ________________________________________________________________________" 
+   |                                                                        |
+   | Shape CLI is a quick source for the latest popular topics in Lifestyle.|
+   |                                                                        |
+   | Want tips and info on beauty, fashion, travel, health, sex, love and   |
+   | everything else you need to live a fuller and happier life?            |
+   |                                                                        |
+   | Enter Y/N                                                              |
+   |________________________________________________________________________|" 
     
   end
 end
