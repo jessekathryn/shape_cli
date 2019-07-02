64c5f060817b170674c2b02d95bc36dee079bde6
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-02 06:05:03 +0000

    cli update and article class update

--- lib/shape_cli/cli.rb
+++ lib/shape_cli/cli.rb
@@ -2,24 +2,25 @@ class ShapeCli::CLI
   
   def home
     puts "
-                                SHAPE CLI 
- ________________________________________________________________________
-|                                                                        |
-| Shape CLI is a quick source for the latest popular topics in Lifestyle.|
-|                                                                        |
-| Want tips and info on beauty, fashion, travel, health, sex, love and   |
-| everything else you need to live a fuller and happier life?            |
-|                                                                        |
-|________________________________________________________________________|" 
+                               SHAPE CLI 
+________________________________________________________________________
+                                                                        
+ Shape CLI is a quick source for the latest popular topics in Lifestyle.
+                                                                        
+ Want tips and info on beauty, fashion, travel, health, sex, love and   
+ everything else you need to live a fuller and happier life?            
+                                                                        
+________________________________________________________________________" 
     menu
   end
 
 
   def menu 
-    puts " ________________________________________________________________________
-|                                                                        |
-|     Y = Yes, display articles / N = No, thank you                      |
-|________________________________________________________________________|"
+    puts 
+"_______________________________________________________________________
+                                                                        
+     Y = Yes, display articles / N = No, thank you                      
+________________________________________________________________________"
      input = nil
       input = gets.strip
         case input
@@ -33,27 +34,28 @@ class ShapeCli::CLI
           puts exit
         else
           puts "
-                                SHAPE CLI 
- _________________________________________________________________________
-|                                                                         |
-|     Y = Yes, display articles / N = No, thank you, let's exit  :        |
-|_________________________________________________________________________|"
+                               SHAPE CLI 
+_________________________________________________________________________
+                                                                         
+     Y = Yes, display articles / N = No, thank you, let's exit  :        
+_________________________________________________________________________"
       end        
     end
     
       
   def display_articles
    puts "
-                                Shape CLI  
+                               Shape CLI  
  _________________________________________________________________________   
-| Enter a number from the list of latest popular topics in Lifestyle      |
-| below to view more details                                              |
-|                                                                         |"   
+ 
+ Enter a number from the list of latest popular topics in Lifestyle      
+ below to view more details                                              
+                                                                          "   
     @article = ShapeCli::Article.popular
     @article.each.with_index(1) do |a, i|
      puts "| |#{i}|  #{a.name}"
     end
-    puts"|_________________________________________________________________________|"
+    puts"________________________________________________________________________"
     display_details
    end
  
@@ -66,21 +68,21 @@ class ShapeCli::CLI
       the_article = @article[input.to_i - 1]
       puts "
                                 SHAPE CLI 
- _________________________________________________________________________
-|                                                                         |
-|   #{the_article.name}                           
-|   #{the_article.summary}                        
-|   #{the_article.url}                            
-|   #{the_article.author}                         
-|   #{the_article.author}                         
-|   #{the_article.date}                           
-|_________________________________________________________________________|"
+_________________________________________________________________________
+   
+   #{the_article.name}                           
+   #{the_article.summary}                        
+   #{the_article.url}                            
+   #{the_article.author}                         
+   #{the_article.author}                         
+   #{the_article.date}                           
+_________________________________________________________________________"
       elsif input == ""
         puts "
- _________________________________________________________________________
-|                                                                         |
-|  Please, enter a number between 1-5 / Type back to return to home~                |
-|_________________________________________________________________________|"
+_________________________________________________________________________
+                                                                        
+  Please, enter a number between 1-5 / Type back to return to home~        
+_________________________________________________________________________"
       elsif input == "exit"
         exit
       else
