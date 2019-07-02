d1062fa9eeb2d7512d73b52f9293f38fbeebc03b
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-02 04:02:08 +0000

    scraper methods for each page w/attr set

--- lib/shape_cli.rb
+++ lib/shape_cli.rb
@@ -1,3 +1,7 @@
+require 'nokogiri'
+require 'pry'
+require 'open-uri'
+
 require_relative "./shape_cli/version"
 require_relative './shape_cli/cli'
 require_relative './shape_cli/article'
--- lib/shape_cli/article.rb
+++ lib/shape_cli/article.rb
@@ -9,7 +9,30 @@ attr_accessor :article, :name, :url, :summary, :date, :author
   def self.scrape_article
     article = []
     
+    article << self.scrape_lifestyle
     
     article
   end  
+  
+  def self.scrape_lifestyle
+    site = "https://www.shape.com/lifestyle"
+    page = Nokogiri::HTML(open(site))
+    binding.pry
+    name = page.css("div.taxonomy-seo-links a").text 
+    url = page.css("div.taxonomy-seo-links a").attr("href").value
+    puts name.text
+  end
+  
+    def self.scrape_article_attr
+    site = "https://www.shape.com/lifestyle/mind-and-body/5-ways-change-your-life-good"
+    page = Nokogiri::HTML(open(site))
+    binding.pry
+    name = page.css("h1.headline").text
+    url = page.css("div.taxonomy-seo-links a").attr("href")
+    summary = page.css("p.dek").text
+    author = page.css("span.bold.author-name").text
+    date = page.css("div.timestamp").text
+    article = page.css("p").text
+    puts name.text
+  end
 end
