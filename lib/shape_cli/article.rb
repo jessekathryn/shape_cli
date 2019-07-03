87b6d8e607d621cfa1e76cb392afde1cdf515075
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-03 04:32:30 +0000

    article class

--- lib/shape_cli/article.rb
+++ lib/shape_cli/article.rb
@@ -1,37 +1,36 @@
 class ShapeCli::Article
   
-attr_accessor :article, :name, :url, :summary, :date, :author
+attr_accessor :article, :name, :url, :summary, :date, :author, :article_text
 
   def self.popular
     self.scrape_article
   end
-  
+
   def self.scrape_article
     article = []
     article << self.scrape_lifestyle
+    article << self.scrape_article_attr
     article
   end  
   
   def self.scrape_lifestyle
     site = "https://www.shape.com/lifestyle"
-    page = Nokogiri::HTML(open(site))
-
-    #name = page.css("div.taxonomy-seo-links div a").text 
-    
+    page = Nokogiri::HTML(open(site)) 
     article = self.new 
     article.name = page.css("div h5 a").text
     article
     end
  
- #need to format these and add a CLI for these attr.
-    def self.scrape_article_attr
-    site = "https://www.shape.com/lifestyle/mind-and-body/5-ways-change-your-life-good"
-    page = Nokogiri::HTML(open(site))
-    name = page.css("h1.headline").text
-    url = page.css("div.taxonomy-seo-links a").attr("href")
-    summary = page.css("p.dek").text
-    author = page.css("span.bold.author-name").text
-    date = page.css("div.timestamp").text
-    article = page.css("p").text
+  def self.scrape_article_attr
+    article.site = "https://www.shape.com/lifestyle/mind-and-body/5-ways-change-your-life-good"
+    article.page = Nokogiri::HTML(open(site))
+    article = self.new
+    article.name = page.css("h1.headline").text
+    article.url = page.css("div.taxonomy-seo-links a").attr("href")
+    article.summary = page.css("p.dek").text
+    article.author = page.css("span.bold.author-name").text
+    article.date = page.css("div.timestamp").text
+    article.article_text = page.css("p").text
+    article
   end
 end
